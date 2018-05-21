1f041e5a51765f340997f4b58b433d099da173bb
SuperVasya <mcjack@inbox.ru>
2018-05-21 14:37:07 +0300

    Add Heroku Procfile

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: python run.py
+clock: python app/clock.py
--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import app
-from app.get_historical_data import get_historical_exchange_rates
+from app.db_methods import post_historical_exchange_rates, post_latest_exchange_rates
 
 
 if __name__ == '__main__':
-    get_historical_exchange_rates('EUR', app.config['HISTORICAL_INTERVAL_DAYS'])
+    post_historical_exchange_rates('EUR', app.config['HISTORICAL_INTERVAL_DAYS'])
     app.run(host="0.0.0.0", port=app.config['PORT'])
