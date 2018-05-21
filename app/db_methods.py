334d435d4f2027ef5fb2723a89f8bf1b5d571a27
SuperVasya <mcjack@inbox.ru>
2018-05-21 14:36:25 +0300

    Combine db methods to one file

--- app/db_methods.py
+++ app/db_methods.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 from app import db
-from app.fixer_api_methods import get_historical_currency_rates
 from app.models import Datapoint
+from app.fixer_api_methods import get_historical_currency_rates, get_current_currency_rates
 
 
-def get_historical_exchange_rates(base_currency_code:str, time_interval_days:int):
+def post_historical_exchange_rates(base_currency_code:str, time_interval_days:int):
     exchange_date = datetime.utcnow().date()
     target_date = exchange_date - timedelta(days=time_interval_days)
     historical_datapoints = Datapoint.query.with_entities(Datapoint.date).\
@@ -25,3 +25,15 @@ def get_historical_exchange_rates(base_currency_code:str, time_interval_days:int
                                       currency_code=currency_code)
                 datapoint.upsert()
             db.session.commit()
+
+
+def post_latest_exchange_rates(base_currency_code:str):
+    exchange_date = datetime.utcnow().date()
+    exchange_rates = get_current_currency_rates(base_currency_code)
+    for currency_code, rate in exchange_rates.items():
+        datapoint = Datapoint(base_currency_code=base_currency_code,
+                              date=exchange_date,
+                              rate=rate,
+                              currency_code=currency_code)
+        datapoint.upsert()
+    db.session.commit()
