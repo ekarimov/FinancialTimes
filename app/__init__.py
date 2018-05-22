22c00cc3f6628eba0ebf8b0c1e288084fd7897c4
SuperVasya <mcjack@inbox.ru>
2018-05-22 09:13:06 +0300

    Change to production config

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ def get_app():
     return Flask(__name__)
 
 app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
+app.config.from_object('config.ProductiontConfig')
 db = SQLAlchemy(app)
 db.init_app(app)
--- config.py
+++ config.py
@@ -27,5 +27,5 @@ class TestingConfig(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     SQLALCHEMY_ECHO = True
     PORT = 5000
-    FIXER_ACCESS_KEY = '871649a063434b259b496d15b9568fb0'
+    FIXER_ACCESS_KEY = os.getenv('FIXER_ACCESS_KEY')
     HISTORICAL_INTERVAL_DAYS = 7
